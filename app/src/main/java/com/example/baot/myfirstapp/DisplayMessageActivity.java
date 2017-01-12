fb6d31bbf7b01105167c87486e80d678111f4a5e
baot20 <baot@baonadeMacBook-Pro.local>
2017-01-12 17:25:18 +0800

    1-12 打卡 添加了图片和点击
    明白了图片为什么显示不出来，然后用alignButton的方式来达到效果

--- app/src/main/java/com/example/baot/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/baot/myfirstapp/DisplayMessageActivity.java
@@ -3,11 +3,20 @@ package com.example.baot.myfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+    int[] images = new int[] {
+            R.drawable.image1,
+            R.drawable.image2,
+    };
+
+    int currentImage=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +25,33 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textForClickTimes = (TextView) findViewById(R.id.textView2);
-        textForClickTimes.setText(message);
+        TextView textFromMain = (TextView) findViewById(R.id.textView2);
+        textFromMain.setText(message);
+        textFromMain.setTextSize(40);
+
+        RelativeLayout main = (RelativeLayout) findViewById(R.id.activity_display_message);
+        final ImageView imageview = new ImageView(this);
+        main.addView(imageview);
+        imageview.setBaselineAlignBottom(true);
+
+        imageview.setImageResource(images[0]);
+        imageview.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                imageview.setImageResource(images[++currentImage%images.length]);
+                if(currentImage>4)imageview.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+        //
 
 
-        /*
+
+
+
+     /*
         TextView textView = new TextView(this);
         textView.setTextSize(40);
         textView.setText(message);
